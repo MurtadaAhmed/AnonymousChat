70b4a8d7431df3fc81b345d87f6a6d9a7c00796c
Murtada Ahmed <murtadhabg@gmail.com>
2024-01-03 18:33:06 +0200

    added the models for the logs

--- chat/models.py
+++ chat/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
 
+class Chat(models.Model):
+    username = models.CharField(max_length=100)
+    message = models.TextField()
+    image = models.ImageField(upload_to='chat_images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
+    group_name = models.CharField(max_length=100, blank=True, null=True)
+
+
 
