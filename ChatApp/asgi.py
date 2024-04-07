119e8127a2113d40e4b530b134b9ad7d3f377caf
Murtada Ahmed <murtadhabg@gmail.com>
2024-04-07 03:20:59 +0300

    added uvicorn support

--- ChatApp/asgi.py
+++ ChatApp/asgi.py
@@ -1,22 +1,21 @@
 import os
 
-from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from chat import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
 
-# application = get_asgi_application()
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 
 
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                routing.websocket_urlpatterns
-            )
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
         )
-    }
-)
+    )
+})
--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'daphne',
+    # 'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,6 +122,8 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
